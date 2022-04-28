64a66cf82489d00a85aa6395abaa65530d0897d7
Casey Harding <casey.r.harding@gmail.com>
2022-04-28 13:02:47 -0400

    Initial test in place.

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -13,13 +13,33 @@ describe("Quotes App", () => {
   const submitBtn = () => cy.get(`button[id="submitBtn"]`);
   const cancelBtn = () => cy.get(`button[id="cancelBtn"]`);
 
+  it("sanity check to make sure tests work", () => {
+    // "it" is a test
+    // "expect" is an assertion
+    // There can be multiple assertions per test,
+    // buuuuut all the assertions have to eal with
+    // the ONE THING that we're testing
+    expect(1 + 2).to.equal(3);
+    expect(2 + 2).not.to.equal(5);
+    expect({}).not.to.equal({}); // passing test? Or failing test? passing test!!!
+    // {} === {} => FALSE
+    expect({}).to.eql({}); // ==
+  })
 
+  it("the proper elements are showing", () => {
+    textInput().should("exist");
+    foobarInput().should("not.exist");
+    authorInput().should("exist");
+    submitBtn().should("exist");
+    cancelBtn().should("exist");
 
+    cy.contains("Submit Quote").should("exist");
+    cy.contains(/submit quote/i).should("exist");
+  })
 
-
-
-
-
+  describe("Filling out the inputs and cancelling", () => {
+    
+  })
 
 
 
