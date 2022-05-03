4974274689c8e3c186ffa24e1d8678e2bd4272ed
timothy hoover <bnicholehilliard@gmail.com>
2022-05-02 20:05:55 -0700

    update

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,42 @@
 // write tests here
+describe('Quotes App', () => {
+    beforeEach(() =>  {
+        //This runs before every test in our testing suite
+        //start each test with fresh state
+        crypto.visit("http:localhost:59522") //Make sure this is the port that's running!
+    })
+
+    //Getters
+    const textInput = () => cy.get("input[name=text]");
+    const authorINput = () => cy.get("input[name=author]");
+    const foobarInput = () => cy.get ("input[name=foobar]");
+    const submitBtn=() => cy.get(`button[id="submitBtn]`);
+    const cancelBtn = () => cy.get(`button[id="cancelBtn"]`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
